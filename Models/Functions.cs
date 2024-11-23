f61b844d0da3f9b4e23d20a4aca7263cd7373f9e
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2024-11-23 19:39:27 +0530

    create DB Connection

---
+++ Models/Functions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BookShop.Models
+{
+    public class Functions
+    {
+        private SqlConnection Con;
+        private SqlCommand cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string ConnString;
+
+        public Functions()
+        {
+            ConnString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Dell\Documents\BookshopDb.mdf;Integrated Security=True;Connect Timeout=30";
+            Con = new SqlConnection(ConnString);
+            cmd = new SqlCommand();
+            cmd.Connection = Con;
+        }
+
+        public DataTable getData(String Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query, ConnString);
+            sda.Fill(dt);
+            return dt;
+        }
+
+        public int SetData(string Query)
+        {
+            int Count = 0;
+            if(Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
+            }
+            cmd.CommandText = Query;
+            Count = cmd.ExecuteNonQuery();
+            Con.Close();
+            return Count;
+        }
+    }
+}
--- View/Admin/Dashboard.aspx
+++ View/Admin/Dashboard.aspx
@@ -2,4 +2,5 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="Mainbody" runat="server">
+    <div><h3>Dashboard</h3></div>
 </asp:Content>
